1edbfa019429a4c368530146e263db75f32e8a55
Sadra Yahyapour <lnxpylnxpy@gmail.com>
2024-07-08 17:39:26 +0330

    updated

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -4,35 +4,63 @@ import { Toggle } from "@/components/ui/toggle"
 
 import BionicReading from "@/utils/bionic"
 
-import {sample, sampleSimplified, sampleSummarized} from "@/samples"
+import {sample, sampleSimplified, sampleSummarized, sampleSummarizedAndSimplified} from "@/samples"
+import { useState } from "react"
+
+import TextTransition, { presets } from 'react-text-transition';
+
 
 
 export default function Demo() {
 
-    return (
-    <section className="w-full py-12 md:py-24 lg:py-32">
-        <div className="container flex flex-col items-center justify-center px-4 md:px-6">
-          <h1 className="text-4xl font-bold mb-4">Try it!</h1>
-          <div className="flex flex-wrap items-center justify-center gap-4 md:flex-nowrap shadow-sm rounded-lg p-2 m-5">
-            <Toggle size="lg" aria-label="Toggle bold" className="gap-2" onClick={()=>alert()}>
-              <LogsIcon className="h-4 w-4" />
-              <span>Sighted</span>
-            </Toggle>
-            <Toggle size="lg" aria-label="Toggle italic" className="gap-2">
-              <WandSparklesIcon className="h-4 w-4" />
-              <span>Simplify</span>
-            </Toggle>
-            <Toggle size="lg" aria-label="Toggle underline" className="gap-2">
-              <ListMinusIcon className="h-4 w-4" />
-              <span>Summarize</span>
-            </Toggle>
-            <Toggle disabled size="lg" aria-label="Toggle strikethrough" className="gap-2">
-              <EraserIcon className="h-4 w-4" />
-              <span>Correct</span>
-            </Toggle>
-          </div>
-            <BionicReading words={sampleSummarized} />
+  const [operation, setOperation] = useState(
+    {
+      "sighted": true,
+      "simplify": false,
+      "summarize": false
+    }
+  )
+
+  var shownText = sample
+
+  if(operation.simplify && operation.summarize)
+    shownText = sampleSummarizedAndSimplified
+  else if (operation.summarize)
+    shownText = sampleSummarized
+  else if (operation.simplify)
+    shownText = sampleSimplified
+
+  return (
+  <section className="w-full py-12 md:py-24 lg:py-32">
+      <div className="container flex flex-col items-center justify-center px-4 md:px-6">
+        <h1 className="text-4xl font-bold mb-8">Your Brain Reads Faster Than Your Eyes!</h1>
+        <div className="flex flex-wrap items-center justify-center gap-4 md:flex-nowrap shadow-sm rounded-lg p-2 m-4">
+          <Toggle size="lg" id="sighted" aria-label="Toggle bold" className="gap-2" pressed={operation.sighted} onPressedChange={() => setOperation({...operation, "sighted": !operation.sighted})}>
+            <LogsIcon className="h-4 w-4" />
+            <span>Sighted</span>
+          </Toggle>
+          <Toggle size="lg" id="simplify" aria-label="Toggle italic" className="gap-2" pressed={operation.simplify} onPressedChange={() => setOperation({...operation, "simplify": !operation.simplify})}>
+            <WandSparklesIcon className="h-4 w-4" />
+            <span>Simplify</span>
+          </Toggle>
+          <Toggle size="lg" id="summarize" aria-label="Toggle underline" className="gap-2" pressed={operation.summarize} onPressedChange={() => setOperation({...operation, "summarize": !operation.summarize})}>
+            <ListMinusIcon className="h-4 w-4" />
+            <span>Summarize</span>
+          </Toggle>
+          <Toggle disabled size="lg" aria-label="Toggle strikethrough" className="gap-2">
+            <EraserIcon className="h-4 w-4" />
+            <span>Correct</span>
+          </Toggle>
         </div>
-      </section>
-    )
+          <TextTransition springConfig={presets.stiff} translateValue="0">
+            {operation.sighted ? 
+            <BionicReading words={shownText}/> : 
+            <p className="mt-8 text-3xl max-w-4xl md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+              {shownText}
+            </p>
+            }
+          </TextTransition>
+      </div>
+    </section>
+  )
 }
--- src/samples.tsx
+++ src/samples.tsx
@@ -1,26 +1,55 @@
-export const sampleDemo = `
-Reading is a fundamental skill that serves as the foundation for learning 
-and personal growth. It opens doors to vast knowledge, 
-providing access to a wealth of information across various subjects and disciplines.
-Through reading, individuals can explore different cultures, histories, and 
-philosophies, broadening their perspectives and enhancing their understanding of the world.
-This continuous intake of knowledge not only aids in academic and professional development 
-but also fosters critical thinking and problem-solving skills, 
-essential attributes in today’s fast-paced, information-driven society.
+export const sample = `
+Bionic Reading, an avant-garde approach to textual consumption, 
+synthesizes ocular kinetics and cognitive processes to amplify 
+comprehension and retention. By strategically emboldening initial 
+letter sequences within words, this methodology exploits the human 
+brain's predilection for pattern recognition, purportedly 
+streamlining the assimilation of complex information.
+
+Intricately interwoven with neurovisual mechanisms, bionic reading 
+endeavors to recalibrate the orthographic and phonological interplay 
+inherent in traditional literacy paradigms. This recalibration ostensibly 
+engenders a more seamless integration of semantic processing, ostensibly 
+enhancing the reader's ability to extrapolate nuanced meanings from intricate 
+texts. Despite its innovative premise, the operationalization of bionic 
+reading within diverse linguistic contexts raises questions about its 
+universal applicability. Additionally, the potential for habituation 
+effects and the cognitive ramifications of sustained exposure to such modified 
+text presentations warrant rigorous longitudinal investigations to elucidate 
+the long-term impacts on reading proficiency and cognitive health.
 `;
 
-export const sampleSimplifiedDemo = `
-Reading is a crucial skill that helps people learn and grow. It provides 
-access to a wide range of information on different topics, expanding knowledge 
-and understanding of the world. This not only supports academic and career development 
-but also improves critical thinking and problem-solving skills.
+export const sampleSimplified = `
+Bionic Reading is a new method to help people read and understand text 
+better by highlighting the first few letters of words. This technique 
+uses the brain’s natural ability to recognize patterns, making it easier 
+to grasp complex information quickly. Supporters believe it reduces mental 
+effort and helps readers get through difficult material faster. However, 
+critics say there’s not enough scientific proof yet to confirm its benefits, 
+and more research is needed.
+
+This reading method works by changing how our eyes and brain interact with text, 
+aiming to make understanding words and meanings easier. While it shows promise, 
+it’s unclear if it works well for all languages and reading styles. There are also 
+concerns about whether people might get too used to this method and what effects it 
+might have on long-term reading skills and mental health. More in-depth studies are 
+necessary to determine its overall impact.
 `
 
 export const sampleSummarized = `
-Reading is essential for learning and personal growth, 
-offering access to a wide range of information and enhancing critical thinking. 
-It benefits the brain by improving focus, memory, and language skills, 
-while also fostering empathy. Additionally, reading provides emotional 
-and psychological relief, offering relaxation and inspiration. Overall, 
-it enriches both the mind and soul, making it a vital part of a fulfilling life.
+Bionic Reading is a new technique that highlights the first few letters of words to 
+help people read and understand text more easily by leveraging the brain's pattern 
+recognition. While it aims to reduce mental effort and speed up reading, its 
+effectiveness lacks sufficient scientific proof, and further research is needed to 
+understand its impact across different languages and long-term effects on reading 
+skills and mental health.
+`
+
+export const sampleSummarizedAndSimplified = `
+Bionic Reading is a technique that highlights the first few letters of words to help 
+people read and understand text more easily by leveraging the brain's pattern 
+recognition. It aims to reduce mental effort and speed up reading, but its effectiveness 
+lacks sufficient scientific proof. More research is needed to confirm its benefits, 
+assess its impact across different languages and reading styles, and understand its 
+long-term effects on reading skills and mental health.
 `
